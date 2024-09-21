096f31bda57e1cc43879ea0dc818ef06a2c824ea
agostina ortellado <ortelladoagos@gmail.com>
2024-09-20 21:49:16 -0300

    .

--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FankyRecords.C_presentacion.Administrador
+//namespace FankyRecords.C_presentacion.Administrador
+namespace FankyRecords.C_presentacion.Administrativo
+
 {
     internal static class Program
     {
