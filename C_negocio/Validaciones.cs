a34fd29c6048dbcbc6a6a4a2b0acb9d11c248ed0
agostina ortellado <ortelladoagos@gmail.com>
2024-09-21 20:27:17 -0300

    agrego la clase validaciones

---
+++ C_negocio/Validaciones.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FankyRecords.C_negocio
+{
+    internal class Validaciones
+    {
+    }
+}
