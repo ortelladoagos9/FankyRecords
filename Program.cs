c3828597837d2088adc86e049b28843f4250e48d
agostina ortellado <ortelladoagos@gmail.com>
2024-09-20 21:53:20 -0300

    .

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FankyRecords
+namespace FankyRecords.C_presentacion.Administrador
 {
     internal static class Program
     {
@@ -17,7 +17,7 @@ namespace FankyRecords
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new formMenuAdmin());
-            Application.Run(new formMenuVendedor());
+            //Application.Run(new formMenuVendedor());
         }
     }
 }
