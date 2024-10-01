e67a98848f46d580edc8309535720197d94ec4a5
Romero Karin <roo_m888@hotmail.com>
2024-09-30 21:34:15 -0300

    Reporte Compras hecho, para verificar validaciones

--- C_presentacion/Administrador/reporteCompras.cs
+++ C_presentacion/Administrador/reporteCompras.cs
@@ -16,5 +16,10 @@ namespace FankyRecords.C_presentacion.Administrador
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
