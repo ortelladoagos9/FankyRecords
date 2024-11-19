af63812c77bb886a1b456a43ba8b5e36990e467f
Romero Karin <roo_m888@hotmail.com>
2024-11-19 17:59:24 -0300

    Entidades venta y detalleventas creadas

---
+++ C_entidad/DetalleVenta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FankyRecords.C_entidad
+{
+    public class DetalleVenta
+    {
+        public int ID_detalleVenta {  get; set; }
+        public decimal PrecioVenta { get; set; }
+        public int Cantidad { get; set; }
+        public decimal SubTotal { get; set; }
+        public Productos Obj_producto { get; set; }
+        public Venta Obj_registrarVenta { get; set; }
+
+    }
+}
---
+++ C_entidad/Venta.cs
@@ -0,0 +1,22 @@
+﻿using FankyRecords.C_presentacion.Modales;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FankyRecords.C_entidad
+{
+    public class Venta
+    {
+        public int ID_ventas { get; set; }
+        public int NumeroFactura { get; set; }
+        public decimal MontoTotal { get; set; }
+        public DateTime FechaVenta { get; set; }
+        public Usuarios Obj_usuarios { get; set; }
+        public Clientes Obj_clientes { get; set; }
+        public TipoDoc Obj_Tipo_Doc { get; set; }
+
+      
+    }
+}
